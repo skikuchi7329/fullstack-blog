5bd9838e06972924490bfb51c84737529542b2e2
Shohei Kikuchi <killchance2256@gmail.com>
2023-08-30 14:53:18 +0900

    feat: 変数化

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,68 @@
-import Image from 'next/image'
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
+import { PostType } from "@/types";
 
+async function fetchBlogs() {
+  const res = await fetch("http://localhost:3000/api/blog", {
+    cache: "no-store",
+  });
 
-export default function Home() {
-  return <main className="w-full h-full">
-  <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
-    <h1 className="text-slate-200 text-center text-2xl font-extrabold">
-      Full Stack Blog 📝
-    </h1>
-  </div>
-  {/* Link */}
-  <div className="flex my-5">
-    <Link
-      href={"/blog/add"}
-      className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-300 font-semibold"
-    >
-      ブログ新規作成
-    </Link>
-  </div>
+  const data = await res.json();
 
-  <div className="w-full flex flex-col justify-center items-center">
-    <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
-      <div className="flex items-center my-3">
-        <div className="mr-auto">
-          <h2 className="mr-auto font-semibold">First Post</h2>
-        </div>
+  return data.posts;
+}
+
+export default async function Home() {
+  const posts = await fetchBlogs();
+  // console.log(posts);
+
+  return (
+    <main className="w-full h-full">
+      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
+        <h1 className="text-slate-200 text-center text-2xl font-extrabold">
+          Full Stack Blog App
+        </h1>
+      </div>
+      {/* Link */}
+      <div className="flex my-5">
         <Link
-          href={`/blog/edit/1`}
-          className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+          href={"/blog/add"}
+          className=" md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-300 font-semibold"
         >
-          編集
+          Add New Blog 🚀
         </Link>
       </div>
 
-      <div className="mr-auto my-1">
-        <blockquote className="font-bold text-slate-700">2023/8/17</blockquote>
-      </div>
+      <div className="w-full flex flex-col justify-center items-center">
+        {posts.map((post: PostType) => (
+          <div
+            key={post.id}
+            className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-200 flex flex-col justify-center"
+          >
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{post.title}</h2>
+              </div>
+              <Link
+                href={`/blog/edit/${post.id}`}
+                className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+              >
+                Edit
+              </Link>
+            </div>
 
-      <div className="mr-auto my-1">
-        <h2>First Post Description</h2>
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">
+                {new Date(post.date).toDateString()}
+              </blockquote>
+            </div>
+
+            <div className="mr-auto my-1">
+              <h2>{post.description}</h2>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
-  </div>
-</main>;
+    </main>
+  );
 }
